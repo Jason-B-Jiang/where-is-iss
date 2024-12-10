ee56bc4bf31bc2a4c145bc8fed12c6d3a03d85e8
Trogdor <jasonbitan.jiang@mail.utoronto.ca>
2024-12-09 19:52:36 -0500

    update lambda function for redshift copy to only copy new data each time, per run date - 1

--- src/update_iss_avg_speed_redshift.py
+++ src/update_iss_avg_speed_redshift.py
@@ -1,14 +1,22 @@
 import boto3
+from datetime import datetime, timedelta
+
 
 def lambda_handler(event, context):
+    datestamp = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+
     query = (
-        f"COPY iss_avg_speed FROM 's3://iss-daily-avg-speed/data/' "
+        f"COPY iss_avg_speed FROM 's3://iss-daily-avg-speed/data/{datestamp}' "
         f"IAM_ROLE 'arn:aws:iam::{boto3.client('sts').get_caller_identity()['Account']}:role/RedshiftNamespaceRole' "
         f"FORMAT AS PARQUET;"
         )
 
-    boto3.client('redshift-data').execute_statement(
+    response = boto3.client('redshift-data').execute_statement(
         Database='dev',
         WorkgroupName='default-workgroup',
-        Sql=query
+        Sql=query,
+        WithEvent=True
     )
+
+    print("Response:", response)
+    boto3.client('redshift-data').describe_statement(Id=response['Id'])
